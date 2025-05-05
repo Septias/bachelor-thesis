0279448c14122a695728faddbbaa1e371044d543
Sebastian Klähn <scoreplayer2000@gmail.com>
2025-05-05 10:54:20 +0200

    use unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Application to set wallpapers from reddit as desktop-background";
   inputs = {
     os_flake.url = "github:septias/nixos-config";
-    nixpkgs.follows = "os_flake/nixpkgs";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -10,9 +9,7 @@
     with inputs;
       flake-utils.lib.eachDefaultSystem (
         system: let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+          pkgs = nixpkgs-unstable.legacyPackages.${system};
         in {
           formatter = pkgs.alejandra;
           devShells.default = pkgs.mkShell {
@@ -20,7 +17,6 @@
               typst
               typstyle
               tinymist
-              typst-lsp
             ];
           };
         }
